a3f19f8d877969c69ce25c0b0f4fb23a1d3dfcbe
Jaydeep-007 <jaydeepvpatil07@gmail.com>
2022-10-02 10:48:39 +0530

    v1

---
+++ GrpcService/Services/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+
+namespace GrpcService.Services
+{
+    public class ProductService : Product.ProductBase
+    {
+        private readonly ILogger<ProductService> _logger;
+        public ProductService(ILogger<ProductService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ProductModel> GetProductsInformation(GetProductDetail request, ServerCallContext context)
+        {
+            ProductModel productDetail = new ProductModel();
+            if (request.ProductId == 1)
+            {
+                productDetail.ProductName = "Samsung TV";
+                productDetail.ProductDescription = "Smart TV";
+                productDetail.ProductPrice = 35000;
+                productDetail.ProductStock = 10;
+            }
+            else if (request.ProductId == 2)
+            {
+                productDetail.ProductName = "HP Laptop";
+                productDetail.ProductDescription = "HP Pavilion";
+                productDetail.ProductPrice = 55000;
+                productDetail.ProductStock = 20;
+            }
+            else if (request.ProductId == 3)
+            {
+                productDetail.ProductName = "IPhone";
+                productDetail.ProductDescription = "IPhone 12";
+                productDetail.ProductPrice = 65000;
+                productDetail.ProductStock = 30;
+            }
+
+            return Task.FromResult(productDetail);
+        }
+    }
+}
---
+++ gRPCClient/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Grpc.Net.Client;
+using GrpcService;
+using GrpcService.Protos;
+
+//var message = new HelloRequest { Name = "Jaydeep" };
+
+//var channel = GrpcChannel.ForAddress("http://localhost:5045");
+//var client = new Greeter.GreeterClient(channel);
+
+//var srerveReply = await client.SayHelloAsync(message);
+
+//Console.WriteLine(srerveReply.Message);
+
+//Console.ReadLine();
+
+var channel = GrpcChannel.ForAddress("http://localhost:5045");
+var client = new Product.ProductClient(channel);
+
+var product = new GetProductDetail { ProductId = 3 };
+
+var serverReply = await client.GetProductsInformationAsync(product);
+
+Console.WriteLine($"{serverReply.ProductName} | {serverReply.ProductDescription} | {serverReply.ProductPrice} | {serverReply.ProductStock}");
+
+Console.ReadLine();
